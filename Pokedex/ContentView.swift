3d1edcbe26e1204a24b5e7b4ec7f6d3469130896
Renan YFC <renanyfc07@gmail.com>
2025-07-15 22:58:05 -0300

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Renan YFC on 15/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
